92f4e77812c08a101670a5287c8ee5536c5f1ac5
Matheus <matheusdiegoteixeira@gmail.com>
2023-08-20 18:35:24 -0300

    commit 20/08

--- app1/bin/app1.dart
+++ app1/bin/app1.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:app1/app1.dart' as app1;
 
 void main(List<String> arguments) {
@@ -61,5 +63,75 @@ void main(List<String> arguments) {
   print(texto_1.replaceAll('xto', 'ste'));
   print(texto_1.split(' ')); // separa a partir do item definido
 
-  print(" Teste de Texto     ".trim()); // remove os espacos ; pode ser Left ou Right tambem
+  print(" Teste de Texto     "
+      .trim()); // remove os espacos ; pode ser Left ou Right tambem
+
+  ///// List
+
+  List<String> lstString = [];
+  var lstInt = [1, 3, 5];
+  List lstDynamic = [];
+
+  // tamanho da lista
+  print(lstInt.length);
+
+  // adicionar items
+  lstString.add('item');
+
+  // remover items
+  lstString.remove('item');
+
+  // verifica se a lista está vazia
+  print(lstDynamic.isEmpty);
+
+  //
+  // funcao Where
+  //
+  print(lstInt.where((x) => x > 3));
+
+  // map
+
+  Map<String, dynamic> map1 = Map<String, dynamic>();
+  var map = {
+    'nome': 'Matheus',
+    'idade': 18,
+    'Hobbies': ['programar', 'tocar instrumentos']
+  };
+
+  print(map['nome']);
+  map.addAll({'cargo': 'programador', 'time': 'portuguesa'});
+  print(map1);
+
+  print(map.containsKey(
+      'nome')); // retorna verdadeiro, pois existe a chave 'nome' no map
+
+  // const
+  const String variavel = 'ABC';
+
+  // dynamic
+  dynamic var1 = 'ABC';
+
+  // DateTime
+
+  DateTime data1 = DateTime.now();
+  DateTime data3 = DateTime.parse('2022-18-02 13:30:35');
+  var data2 = DateTime.tryParse('2022-02-01 00:00:00');
+  
+
+  // é possivel tirar as partes da data
+  print(data1.day +
+      data1.month +
+      data1.year +
+      data1.hour +
+      data1.minute +
+      data1.second +
+      data1.weekday);
+
+  // Operações com datas !!!
+
+  data1.add(Duration(days: 1));
+  data1.subtract(Duration(days: 1));
+  print(data1.isAfter(data3));
+  print(data1.isBefore(data3));
+  print(data1.compareTo(data3)); // -1 se antes, 0 se igual e 1 se depois
 }
