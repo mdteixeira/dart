6e8b1727c00092fe11c79524e9ba73380220e033
Matheus <matheusdiegoteixeira@gmail.com>
2023-08-22 16:45:34 -0300

    while

--- app1/lib/mod3.dart
+++ app1/lib/mod3.dart
@@ -1,14 +1,51 @@
+import 'dart:convert';
+import 'dart:io';
+
 void mod3() {
-  for (var i = 0; i < 5; i++) {
-    print(i);
+  // for (var i = 0; i < 5; i++) {
+  //   print(i);
+  // }
+
+  // var texto = 'string';
+  // for (var i = 0; i < texto.length; i++) {
+  //   print(texto[i]);
+  // }
+  // var listaNomes = ['Matheus', 'Duda', 'Renato'];
+  // for (var i = 0; i < listaNomes.length; i++) {
+  //   print(listaNomes[i]);
+  // }
+
+  var numeros = [1, 5, 6, 56, 5, 7, 8, 98];
+
+  var acumulador = 0;
+  for (var numero in numeros) {
+    acumulador = acumulador + numero;
+    print(acumulador);
+  }
+  var letras = ['a', 'c', 'm', 'n', 'k'];
+  for (var letra in letras) {
+    print(letra);
   }
 
-  var texto = 'string';
-  for (var i = 0; i < texto.length; i++) {
-    print(texto[i]);
+  letras.forEach((element) {
+    print((element));
+  });
+
+  var i = 0;
+  var total = 10;
+  while (i < total) {
+    print(i);
+    i++;
   }
-  var listaNomes = ['Matheus', 'Duda', 'Renato'];
-  for (var i = 0; i < listaNomes.length; i++) {
-    print(listaNomes[i]);
+
+  print("Digite um número ou 'S' para sair");
+  var line = stdin.readLineSync(encoding: utf8);
+  double j = 0;
+  while (line != 'S') {
+    var numero = double.parse(line ?? '');
+    j = j + numero;
+    print("Digite um número ou 'S' para sair");
+    line = stdin.readLineSync(encoding: utf8);
   }
+  print(j);
 }
