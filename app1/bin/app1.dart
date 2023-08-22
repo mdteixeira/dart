45824029cf841dfb765dc0645c10a58618d298d3
Matheus <matheusdiegoteixeira@gmail.com>
2023-08-22 16:18:15 -0300

    for

--- app1/bin/app1.dart
+++ app1/bin/app1.dart
@@ -1,9 +1,11 @@
 import 'package:app1/calculadora.dart';
 import 'package:app1/mod1.dart';
 import 'package:app1/mod2.dart';
+import 'package:app1/mod3.dart';
 
 void main(List<String> arguments) {
   // mod1();
   // mod2();
-  calculadora();
+  // calculadora();
+  mod3();
 }
---
+++ app1/lib/mod3.dart
@@ -0,0 +1,14 @@
+void mod3() {
+  for (var i = 0; i < 5; i++) {
+    print(i);
+  }
+
+  var texto = 'string';
+  for (var i = 0; i < texto.length; i++) {
+    print(texto[i]);
+  }
+  var listaNomes = ['Matheus', 'Duda', 'Renato'];
+  for (var i = 0; i < listaNomes.length; i++) {
+    print(listaNomes[i]);
+  }
+}
